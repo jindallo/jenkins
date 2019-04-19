f8cb55aea306a0604192a23a405514987de50e35
Jin Hsieh <zhouting.xie.o@nio.com>
2019-04-19 16:34:07 +0800

    Test 35

--- test_calibration.py
+++ test_calibration.py
@@ -1,8 +1,10 @@
 from asimov import s32v
 
 class Test():
-    def test_read_calibration():
-        print("1")
+    def test_read_calibration(self):
+        print("[Jin] 1")
+        assert 1 == 1
 
-    def test_read_calibration_2():
-        print("2")
+    def test_read_calibration_2(self):
+        print("[Jin] 2")
+        assert 1 == 1
